b2adfc9af4aa566307818b451a26f97754e02427
Diego <diegoasp18@gmail.com>
2020-05-17 19:46:27 -0300

    Added student name extract + linting

--- extrae_notas_uade.py
+++ extrae_notas_uade.py
@@ -1,31 +1,39 @@
+'''
+Grades extractor from the UADE WebCampus2 website
+'''
+
+import sys
+import os
+from datetime import datetime
+
 try:
     from bs4 import BeautifulSoup
     from selenium import webdriver
     from pandas import ExcelWriter, DataFrame
-    from datetime import datetime
-    import os
 
-except ImportError:
-    print('*Ocurrio un error al importar los modulos necesarios para este programa.')
-    print('\nAsegurese de tener instalados por medio de "pip install ..." los siguientes modulos:\n\
--beautifulsoup4\n-selenium\n-pandas\n')
-    exit()
+except ImportError as error:
+    MODS = ['beautifulsoup4', 'pandas', 'selenium']
+    print(f'Error: {error}')
+    print('\n*Ocurrio un error al importar los modulos necesarios.')
+    print('Asegurese de tener instalados por "pip install ..." los modulos:\n')
+    for mod in MODS:
+        print('-' + mod)
+    sys.exit()
 
 def opcion(texto='Si o no?: '):
     'Pregunta opcion, devuelve 1 para positivo, 0 para negativo'
-    si = ['si', 's', '1']
-    no = ['no', 'n', '0']
+    positivo = ['si', 's', '1']
+    negativo = ['no', 'n', '0']
     while True:
-        op = input(texto).lower()
-        if op in si:
-            x = 1
+        ingreso = input(texto).lower()
+        if ingreso in positivo:
+            resp = 1
             break
-        elif op in no:
-            x = 0
+        if ingreso in negativo:
+            resp = 0
             break
-        else:
-            print('Si o no?\n')
-    return x
+        print('Si o no?\n')
+    return resp
 
 def timer(func):
     'Prints function runtime'
@@ -34,122 +42,145 @@ def timer(func):
         func()
         time2 = datetime.now()
         print(f'Runtime: {time2 - time1}')
-        
+
     return wrapper
 
 def login(driver, url, usr, psw):
     '''Logs in to the UADE webcampus profile
-    
+
     Receives oper chromedriver.exe, login page url, user and password'''
-    if not psw.endswith('\n'): psw += '\n'
+    if not psw.endswith('\n'):
+        psw += '\n'
     driver.get(url)
     driver.find_element_by_id('ctl00_ContentPlaceHolderMain_txtUser').send_keys(usr)
     driver.find_element_by_id('ctl00_ContentPlaceHolderMain_txtClave1').send_keys(psw)
-    driver.get(driver.current_url)
-    if driver.current_url == url:
-        raise PermissionError("Login failed. Please verify that you're using the correct user/password.")
+    driver.get(driver.current_url) # waits for the new url to load before proceeding
+    if driver.current_url == url:  # if no page change is detected
+        raise PermissionError("'Login failed. Please verify that you're using the \
+correct user/password.'")
 
 def logout(driver):
     'Signs off UADE user'
     driver.find_element_by_id('ctl00_Top1_lnkCerrarSesion').click()
 
+def kill_driver(driver):
+    'Close the open chromedriver'
+    driver.close()
+
 def kill():
     'Kill the script exceution'
     print('\n\n* Se finalizara el programa.')
-    exit()
+    sys.exit()
 
-def notesExtract(table):
+def extract_notes(table):
     '''Extract grades from the "cuatrimestre" page
-    
-    Table = HTML table grades section'''
+
+    Table = HTML table grades section
+    '''
     grades = []
     for grade in table.findAll('td', class_='td-texbox'):
         grades.append(grade.text)
     return grades
-    
-def classInfoExtract(driver, url):
-    '''Extract the info from the "cuatrimestre" page
-    
-    Returns classes (list), grades (matrix n_rows = len(classes))
-    period (str with name of semester).'''
 
+def extract_class_info(driver, url):
+    '''Extract the info from the "cuatrimestre" page
+
+    Returns classes (list), grades (matrix n_rows = len(classes))
+    period (str with name of semester).
+    '''
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, features="html.parser")
-    
+
     classes = []
     grades_list = []
-
     period = soup.find('tr', class_='td-ADMdoc-REG').text # Name of semester
-    
+
     for classroom in soup.findAll('tr', class_="td-AULA-bkg"):  # Classroom table
-        name = classroom.find('a').text
-        
-        grades_table = classroom.find('td', class_='tabla-ID2').findAll('tr')[1] # Position 1 has the grades, position 0 the header
-        grades = notesExtract(grades_table)
+        name = classroom.find('a').text.split('-')[0].strip()
+        grades_table = classroom.find('td', class_='tabla-ID2').findAll('tr')[1]
+        # Position 1 has the grades, position 0 the header
+        grades = extract_notes(grades_table)
 
         classes.append(name)
         grades_list.append(grades)
     return classes, grades_list, period
 
-def createExcel(class_matrix, header, file_name='Notas_UADE.xlsx'):
+def create_excel(class_matrix, header, student, file_name='Notas_UADE.xlsx'):
     '''Exports Class info to xlsx, requires pandas' ExcelWriter and DataFrame
-    
+
     Receives:
     - class_matrix: each row containing: [class_info (names list), grades (matrix),
     sheet (str with desired excel sheet name), title (str with title for the first cel)
     - header: list containing the titles for the grades table
+    - student: str, student name
     - file_name: optional, default = 'Notas_UADE.xlsx
     '''
-
     with ExcelWriter(file_name, mode='w+') as writer:
         for row in class_matrix:
-            class_info, grades, sheet, title = row 
+            class_info, grades, sheet, title = row
 
             class_data = DataFrame(data=grades, index=class_info, columns=header)
             class_data.to_excel(writer, sheet_name=sheet, startrow=3)
 
             sheet = writer.sheets[sheet]
-            sheet.cell(row=1, column=1).value = title
+            sheet.cell(row=1, column=1).value = f'Alumno: {student}'
+            sheet.cell(row=2, column=1).value = title
     writer.close()
 
-def classMatrixCreate(driver, links):
+def create_class_matrix(driver, links):
     ''' Create class info matrix: each row containing: [class_info (names list), grades (matrix),
     sheet (str with desired excel sheet name), title (str with title for the first cel)
     '''
     matrix = []
     for link in links:
         url = r'https://www.webcampus.uade.edu.ar/' + link
-        clases, grades, period = classInfoExtract(driver, url)
+        clases, grades, period = extract_class_info(driver, url)
         page_name = period.split('-')[1].strip()
 
-        if 'Cuatrimestre' in page_name: page_name.replace('Cuatrimestre', 'Cuatr.')
-        
+        if 'Cuatrimestre' in page_name:
+            page_name.replace('Cuatrimestre', 'Cuatr.')
+
         matrix.append([clases, grades, page_name, period])
     return matrix
 
-def sameType(matrix, col):
+def same_type(matrix, col):
     "Checks that every item in matrix's column is the same type."
     prev_item = matrix[0][col]
     for i in range(1, len(matrix)):
-        if type(matrix[i][col]) != type(prev_item):
+        if not isinstance(matrix[i][col], type(prev_item)):
             break
         prev_item = matrix[i][col]
     else:
         return True
     return False
-    
-def matrizxIsUniform(matrix):
+
+def uniform_matrix(matrix):
     "Determines if a given matrix contains uniform data types"
     cols = len(matrix[0])
     for i in range(cols):
-        if not sameType(matrix, i):
+        if not same_type(matrix, i):
             break
     else:
         return True
     return False
-        
 
-def semestersExtract(driver):
+def name_extract(driver):
+    """Receives chromedriver on webcampus' homepage
+    Returns name, lastname. (both str)
+    """
+    soup = BeautifulSoup(driver.page_source, features="html.parser")
+    name = soup.find('span', class_="TOPnombre").text.strip('Bienvenido')
+    return name
+
+def create_filename(name):
+    'Creates the name for the excel file'
+    lastname, name = [x.strip() for x in name.split(',')]
+    f_name = name.split()[0].title()
+    l_name = lastname.split()[0].title()
+    file_name = f'{f_name}_{l_name}_Notas_UADE.xlsx'
+    return file_name
+
+def extract_links(driver):
     "Extract current and past semesters urls from webcampus' home"
     links = []
     soup = BeautifulSoup(driver.page_source, features="html.parser")
@@ -168,8 +199,9 @@ def __main__():
     url = r'https://www.webcampus.uade.edu.ar/Login.aspx'
     driver_path = r"C:\Apps\chromedriver_win32\chromedriver.exe"
     while not os.path.isfile(driver_path):
-        print(f'El webdriver de chrome no se encotro en la direccion predeterminada "{driver_path}"\n')
-        print(r'Link para la descarga del chrome web driver: https://sites.google.com/a/chromium.org/chromedriver/home')
+        driver_website = r"https://sites.google.com/a/chromium.org/chromedriver/home"
+        print(f'El webdriver de chrome no se encotro en la direccion "{driver_path}"\n')
+        print(f'Link para la descarga del chrome web driver: "{driver_website}"')
 
         if opcion('\nDesea ingresar la direccion manualmente?: '):
             driver_path = input("Ingrese la direccion completa del chromedriver.exe: ")
@@ -188,7 +220,7 @@ def __main__():
                 print(f'No se encontro el archivo "{creds_file}".')
                 print(f'\nSe recomienda crear un archivo llamado "{creds_file}" que contenga\
                     \nel usuario y la contraseña separados por un punto y coma ";"\n')
-                
+
                 if opcion("Desea ingresarlos manualmente?: "):
                     usr = input('Ingrese su usuario: ')
                     psw = input('Ingrese su contraseña: ')
@@ -198,30 +230,37 @@ def __main__():
                 login(driver, url, usr, psw)
                 break
 
-            except PermissionError as e:
-                print(f'\n* El inicio de sesion fallo: "{str(e)}"')
+            except PermissionError as error:
+                print(f'\n* El inicio de sesion fallo: {error}')
                 if opcion('Desea intentarlo de nuevo?: '):
                     continue
-                else:
-                    kill()
+                kill_driver(driver)
+                kill()
 
-        links = semestersExtract(driver)
+        st_name = name_extract(driver) # Studen name format lastname, name
+        excel_filename = create_filename(st_name)
+        links = extract_links(driver)
         if links != []:
-            header =  ['Parcial 1', 'Parcial 2', 'Recup', 'TP', 'Cursada', 'Examen Final', 'Condicion Final', 'Asistencia']
-            class_matrix = classMatrixCreate(driver, links)
+            header = [
+                'Parcial 1', 'Parcial 2', 'Recup', 'TP', 'Cursada',
+                'Examen Final', 'Condicion Final', 'Asistencia'
+                ]
+            class_matrix = create_class_matrix(driver, links)
             logout(driver)
+            kill_driver(driver)
 
-            if matrizxIsUniform(class_matrix):
-                createExcel(class_matrix, header)
+            if uniform_matrix(class_matrix):
+                create_excel(class_matrix, header, st_name, file_name=excel_filename)
             else:
                 raise RuntimeError('Ocurrio un error al extraer la info de las cursadas.')
         else:
             raise RuntimeError('No se logro extraer ningun enlace a cuatrimestre cursado.')
-
-    except RuntimeError as e:
-        print(f'> Ocurrio un error durante la ejecucion del programa: "{str(e)}"')
-    except Exception as e:
-        print(f'> Error fatal inesperado: "{str(e)}"')
+    except PermissionError as error:
+        print(f'> Ocurrio un error al momento de grabar el excel: {error}')
+    except RuntimeError as error:
+        print(f'> Ocurrio un error durante la ejecucion del programa: {error}')
+    except Exception as error:
+        print(f'> Error fatal inesperado: {error}')
     else:
         print('El programa fue ejecutado con exito!')
 
